660b060a07d4dc28f315a004e1d6ce695da5b13e
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2019-07-23 07:04:38 +0100

    fix broken url

--- arrays/array_manipulation.py
+++ arrays/array_manipulation.py
@@ -1,4 +1,4 @@
-# http://hr.gs/deabde 
+# https://www.hackerrank.com/challenges/crush/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=arrays
 
 import unittest
 
