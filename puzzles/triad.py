ddb9744992717f64b7b594db429cf4c30ca02f3a
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2020-04-07 22:40:45 +0100

    Works upto n=11

---
+++ puzzles/triad.py
@@ -0,0 +1,171 @@
+# https://www.hackerrank.com/challenges/ctci-fibonacci-numbers/problem
+
+import unittest
+
+
+def create_triangle(n):
+    triangle = []
+    for row in range(n):
+        output_row = []
+        for column in range(row+1):
+            output_row.append(column + 1)
+        triangle.append(output_row)
+    return triangle
+
+
+def print_triangle(triangle):
+    n = len(triangle)
+    for i, row in enumerate(triangle):
+        output = []
+        output.append(" " * ((n-i)*2))
+        for column in row:
+            output.append(str(column))
+        print("   ".join(output))
+
+
+def is_triadic(n):
+    state = create_triangle(n)
+    for i, row in enumerate(state[:-1]):
+        for j, column in enumerate(row):
+            print("row ", i)
+            print("column ", j)
+
+            if state[i][j]:
+                state[i][j] = 0
+                # if not the 3rd last row or the 1st row
+                if i != n - 3 or i == 0:
+                    # fill in - start with upside down triangle
+                    # except:
+                    # 1st element, last element
+                    if j != 0 and i != j and state[i][j+1] and state[i+1][j+1] and i != n - 2:
+                        state = draw_upside_down_triad(state, i, j)
+                    # normal triangle possible?
+                    elif state[i+1][j] and state[i+1][j+1]:
+                        state = draw_normal_triad(state, i, j)
+                    # upside down triangle possible?
+                    elif state[i][j+1] and state[i+1][j+1]:
+                        state = draw_upside_down_triad(state, i, j)
+                # if 3rd last row
+                else:
+                    # it's the last point in the row and it's not the last row
+                    if i == j and i != n - 1:
+                        state = draw_normal_triad(state, i, j)
+                    # upside down triangle possible?
+                    elif state[i][j+1] and state[i+1][j+1]:
+                        print("Watch out here!")
+                        state = draw_upside_down_triad(state, i, j, False)
+                    else:
+                        return False, state
+            else:
+                continue
+            print_triangle(state)
+    if state[n-1][n-1]:
+        return False, state
+    else:
+        return True, state
+
+
+def draw_normal_triad(state, i, j):
+    state[i+1][j] = 0
+    state[i+1][j+1] = 0
+    return state
+
+
+def draw_upside_down_triad(state, i, j, value=0):
+    state[i][j+1] = 0
+    state[i+1][j+1] = 0
+    return state
+
+
+class MyTest(unittest.TestCase):
+    def test_1(self):
+        n = 2
+        received = is_triadic(n)
+        expected = True, [[0], [0, 0]]
+        self.assertEqual(received, expected)
+
+    def test_3(self):
+        n = 3
+        received = is_triadic(n)
+        expected = False, [[0], [0, 0], [1, 2, 3]]
+        self.assertEqual(received, expected)
+
+    def test_4(self):
+        n = 4
+        received = is_triadic(n)
+        expected = False, [[0], [0, 0], [0, 0, 0], [0, 0, 0, 4]]
+        self.assertEqual(received, expected)
+
+    def test_5(self):
+        n = 5
+        received = is_triadic(n)
+        expected = False, [[0], [0, 0], [0, 0, 0],
+                           [0, 0, 0, 0], [0, 0, 3, 4, 5]]
+        self.assertEqual(received, expected)
+
+    def test_6(self):
+        n = 6
+        received = is_triadic(n)
+        expected = False, [[0], [0, 0], [0, 0, 0], [
+            0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 4, 5, 6]]
+        self.assertEqual(received, expected)
+
+    def test_7(self):
+        n = 7
+        received = is_triadic(n)
+        expected = False, [[0], [0, 0], [0, 0, 0], [
+            0, 0, 0, 0], [0, 0, 0, 0, 0], [1, 0, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6, 7]]
+        self.assertEqual(received, expected)
+
+    def test_8(self):
+        n = 8
+        received = is_triadic(n)
+        expected = False, [[0], [0, 0], [0, 0, 0], [
+            0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 6, 7, 8]]
+        self.assertEqual(received, expected)
+
+    def test_9(self):
+        n = 9
+        received = is_triadic(n)
+        expected = True
+        self.assertEqual(received[0], expected)
+
+    def test_10(self):
+        n = 10
+        received = is_triadic(n)
+        expected = False
+        self.assertEqual(received[0], expected)
+
+    def test_11(self):
+        n = 11
+        received = is_triadic(n)
+        expected = True
+        self.assertEqual(received[0], expected)
+
+    def test_12(self):
+        n = 12
+        received = is_triadic(n)
+        expected = True
+        self.assertEqual(received[0], expected)
+
+    # def test_13(self):
+    #     n = 13
+    #     received = is_triadic(n)
+    #     expected = False
+    #     self.assertEqual(received[0], expected)
+
+    # def test_14(self):
+    #     n = 14
+    #     received = is_triadic(n)
+    #     expected = False
+    #     self.assertEqual(received[0], expected)
+
+    # def test_15(self):
+    #     n = 15
+    #     received = is_triadic(n)
+    #     expected = True
+    #     self.assertEqual(received[0], expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
