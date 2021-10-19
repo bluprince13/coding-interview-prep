ef7145ed10cc5567e625ec629410f999f99cf216
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2021-10-19 22:43:42 +0530

    balanced-brackets solved with Geethu

---
+++ stacks_and_queues/balanced_brackets.py
@@ -0,0 +1,65 @@
+# https://www.hackerrank.com/challenges/balanced-brackets
+
+import unittest
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'isBalanced' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+# Time complexity - O(n)
+# Space complexity - O(n)
+
+def isBalanced(s):
+    stack  = []
+    openBrackets = "({["
+    closedBrackets = ")}]"
+
+    for character in s:
+        if character in openBrackets:
+            stack.append(character)
+        elif stack and character in closedBrackets:
+            lastCharacterInStack = stack[-1]
+            if openBrackets.find(lastCharacterInStack) == closedBrackets.find(character):
+                stack.pop()
+            else:
+                return "NO"
+        else:
+            # if the stack is empty and the character is a closing bracket
+            return "NO"
+
+    # check if the stack has open brackets left
+    if stack:
+        return "NO"
+    else:
+        return "YES"
+
+class MyTest(unittest.TestCase):
+    def test_1(self):
+        text = "{[()]}"
+        received = isBalanced(text)
+        expected = "YES"
+        self.assertEqual(received, expected)
+
+    def test_2(self):
+        text = "{[(])}"
+        received = isBalanced(text)
+        expected = "NO"
+        self.assertEqual(received, expected)
+
+    def test_3(self):
+        text = "{{[[(())]]}}"
+        received = isBalanced(text)
+        expected = "YES"
+        self.assertEqual(received, expected)
+
+if __name__ == '__main__':
+    unittest.main()
