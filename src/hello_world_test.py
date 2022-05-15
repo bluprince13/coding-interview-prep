d5376e7be9d281c8fde780ada702c48e7eba0b6b
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2022-05-15 10:42:47 +0100

    Add hello_world test template for Python

---
+++ src/hello_world_test.py
@@ -0,0 +1,36 @@
+import pytest
+from dataclasses import dataclass
+
+def say_hello(name: str) -> str:
+    return f"Hello {name}"
+
+# ------------------------------------------------------------------------------
+# TESTS
+# ------------------------------------------------------------------------------
+
+@dataclass
+class TestData:
+    id: str
+    input: tuple
+    expected: any
+    __test__ = False
+
+
+class TestClass:
+    testdata = [
+        TestData(id="boy name", input=("Vipin",), expected="Hello Vipin"),
+        TestData(id="girl name", input=("Geethu",), expected="FAIL"),
+    ]
+
+    @pytest.mark.parametrize(
+        "input,expected",
+        map(lambda x: (x.input, x.expected), testdata),
+        ids=map(lambda x: x.id, testdata),
+    )
+    def test(self, input, expected):
+        actual = say_hello(*input)
+        assert actual == expected
+
+
+if __name__ == "__main__":
+    pytest.main()
