d9ddcfa6c7ccb75dbc7892d214e647a35670660d
Vipin Ajayakumar <vipinaj@amazon.co.uk>
2023-04-12 22:42:32 +0100

    Finish simplify_path

--- src/stacks_and_queues/simplify_path.rs
+++ src/stacks_and_queues/simplify_path.rs
@@ -4,15 +4,19 @@ pub fn simplify_path(path: String) -> String {
     let elements = path.split("/");
     let mut stack = vec![""];
     for element in elements {
-        if element != "" {
+        if element == ".." {
+            if stack.len() > 1 {
+                stack.pop();
+            };
+        } else if (element != "") && (element != ".") {
             stack.push(element)
         }
     }
 
-    let length = stack.len();
-    if (length > 1) & (stack[length - 1] == "/") {
-        stack.pop();
+    if stack.len() == 1 {
+        stack.push("");
     }
+
     stack.join("/")
 }
 
@@ -21,14 +25,43 @@ mod tests {
     use super::*;
 
     #[test]
-    fn simplify_path_returns_path_1() {
+    fn simplify_path_removes_trailing_slash() {
         let actual = simplify_path("/home/".to_string());
 
         assert_eq!(actual, "/home");
     }
 
-    fn simplify_path_returns_path_2() {
-        let actual = simplify_path("/home/../src/test/".to_string());
+    #[test]
+    fn simplify_path_handles_initial_double_period() {
+        let actual = simplify_path("/../".to_string());
+
+        assert_eq!(actual, "/");
+    }
+
+    #[test]
+    fn simplify_path_handles_later_double_period() {
+        let actual = simplify_path("/home/../".to_string());
+
+        assert_eq!(actual, "/");
+    }
+
+    #[test]
+    fn simplify_path_handles_period() {
+        let actual = simplify_path("/.".to_string());
+
+        assert_eq!(actual, "/");
+    }
+
+    #[test]
+    fn simplify_path_handles_multiple_slash() {
+        let actual = simplify_path("home//foo".to_string());
+
+        assert_eq!(actual, "/home/foo");
+    }
+
+    #[test]
+    fn simplify_path_handles_complex_input() {
+        let actual = simplify_path("home/../src//./test/".to_string());
 
         assert_eq!(actual, "/src/test");
     }
