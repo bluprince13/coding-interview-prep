f3740e16f8615ff01d2c924897d8bedfb2d5b4d2
Vipin Ajayakumar <vipinaj@amazon.co.uk>
2023-04-23 23:06:39 +0100

    Convert to static build

--- webapp/svelte.config.js
+++ webapp/svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
