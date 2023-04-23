9dce45944268ed2d08ee768b4aa685887d97ee2a
Vipin Ajayakumar <vipinaj@amazon.co.uk>
2023-04-23 22:24:25 +0100

    Make standard

--- webapp/svelte.config.js
+++ webapp/svelte.config.js
@@ -1,5 +1,5 @@
 import { vitePreprocess } from '@sveltejs/kit/vite';
-import adapter from '@sveltejs/adapter-netlify';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
