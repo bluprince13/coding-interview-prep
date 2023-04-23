65c3cc53fe36ba109ae602ae1cb98f5b2ec258a2
Vipin Ajayakumar <vipinaj@amazon.co.uk>
2023-04-23 19:24:45 +0100

    Add svelte webapp with netlify config and a basic table

---
+++ webapp/src/routes/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    import problemsJson from '../buildAssets/problems.json';
+    import type { Problem } from '../buildAssets/bootstrapWebapp';
+    const problems: Problem[] = problemsJson
+</script>
+
+<h1>Coding problems</h1>
+<table>
+	<thead>
+		<tr>
+            <th>Number</th>
+			<th>File</th>
+            <th>Language</th>
+			<th>Problem Source</th>
+			<th>Difficulty</th>
+			<th>Perceived difficulty</th>
+			<th>Categories</th>
+			<th>Created</th>
+			<th>Modified</th>
+		</tr>
+	</thead>
+	<tbody>
+		{#each problems as problem}
+			<tr>
+				<td>{problem.number}</td>
+				<td><a href={problem.fileUrl}>{problem.fileName}</a></td>
+                <td>{problem.language}</td>
+				<td><a href={problem.metadata.url}>{problem.metadata.linkText}</a></td>
+				<td>{problem.metadata.difficulty.site}</td>
+				<td>{problem.metadata.difficulty.perceived}</td>
+				<td>{problem.metadata.categories.join(', ')}</td>
+				<td>{new Date(problem.createdAt).toLocaleDateString()}</td>
+				<td>{new Date(problem.modifiedAt).toLocaleDateString()}</td>
+			</tr>
+		{/each}
+	</tbody>
+</table>
+
+<style>
+	table {
+		border-collapse: collapse;
+		width: 100%;
+	}
+
+	th,
+	td {
+		border: 1px solid black;
+		padding: 8px;
+		text-align: left;
+	}
+
+	th {
+		background-color: #dddddd;
+	}
+</style>
