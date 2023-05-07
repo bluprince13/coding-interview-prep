79de54ee050a97a23eea86a45dc037be9c166027
Vipin Ajayakumar <vipinaj@amazon.co.uk>
2023-05-08 00:34:58 +0100

    WIP

--- bootstrapWebapp.ts
+++ bootstrapWebapp.ts
@@ -4,6 +4,7 @@ import { load } from 'js-yaml'
 import path from 'path'
 import recursiveReaddir from 'recursive-readdir'
 import { Octokit } from '@octokit/rest'
+import { OctokitResponse } from '@octokit/types'
 
 export interface ProblemMetadata {
     url: string
@@ -118,58 +119,85 @@ export const getProblems = async (): Promise<Problem[]> => {
     return problems
 }
 
-const buildAssetsDirectory = 'webapp/src/buildAssets';
+const buildAssetsDirectory = 'webapp/src/buildAssets'
 if (!fs.existsSync(buildAssetsDirectory)) {
-  fs.mkdirSync(buildAssetsDirectory, { recursive: true });
-  console.log(`Created directory ${buildAssetsDirectory}`);
+    fs.mkdirSync(buildAssetsDirectory, { recursive: true })
+    console.log(`Created directory ${buildAssetsDirectory}`)
 } else {
-  console.log(`Directory ${buildAssetsDirectory} already exists`);
+    console.log(`Directory ${buildAssetsDirectory} already exists`)
+}
+
 interface FilesAndContents {
     [key: string]: string
 }
 
-const octokit = new Octokit({ auth: `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` });
-let dirContentsConfig = {
-    owner: "bluprince13",
-    repo: "coding-interview-prep",
-    path: "src"
-  }
+const octokit = new Octokit({
+    auth: `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`
+})
 
-const getFilesAndContents = async () : Promise<FilesAndContents> => {
-    const githubContents = await octokit.repos.getContent(dirContentsConfig);
-    const files = githubContents.data
-    const filesAndContents: FilesAndContents= {}
-    await Promise.all(
-        files.forEach(async (file: any) => {
-            const contentsResponse = await axios.get(file.download_url)
-            const contents = contentsResponse.data
-            filesAndContents[file.name] = contents
-        })
-    )
+const getFilesAndContents = async (): Promise<FilesAndContents> => {
+    const filesAndContents: FilesAndContents = {}
+    try {
+        const response = await octokit.request(
+            'GET /repos/{owner}/{repo}/git/trees/{tree_sha}',
+            {
+                owner: 'bluprince13',
+                repo: 'coding-interview-prep',
+                path: 'src',
+                tree_sha: 'master',
+                recursive: 'true'
+            }
+        )
+        let files = response.data.tree as any[]
+        files = files.filter((file) =>
+            ['.rs', '.py', '.go', '.ts'].some(
+                (ext) => file.path.endsWith(ext) && file.path.startsWith('src')
+            )
+        )
+        await Promise.all(
+            [files[0]].map(async (file: any) => {
+                const contentsResponse = await octokit.repos.getContent({
+                    owner: 'bluprince13',
+                    repo: 'coding-interview-prep',
+                    path: file.path
+                })
+                const contents = contentsResponse.data as unknown as string
+                console.log(file)
+                console.log(file.path)
+                console.log(contents)
+                filesAndContents[file.name] = contents
+            })
+        )
+    } catch (e) {
+        console.log(e)
+    }
     return filesAndContents
 }
-
-export const getProblems = async (filesAndContents: FilesAndContents): Promise<Problem[]> => {
-    const problems = Object.entries(filesAndContents).map(
-        ([file, contents]) => {
-            const [, metadataStr] = contents.split('\n---')
-            if (!metadataStr) return null
-            const metadata = load(metadataStr.trim()) as ProblemMetadata
-            return {
-                filename: file.name,
-                metadata,
-                createdAt: new Date(file.created_at);
-                modifiedAt: new Date(file.updated_at);
-            }
-        }
-    )
-    return problems.filter((problem) => !!problem)
-}
-
-getProblems().then((problems) => {
-    const jsonData = JSON.stringify(problems)
-    fs.writeFileSync(`${buildAssetsDirectory}/problems.json`, jsonData)
+getFilesAndContents().then(() => {
+    console.log('Done')
 })
+
+// export const getProblems = async (filesAndContents: FilesAndContents): Promise<Problem[]> => {
+//     const problems = Object.entries(filesAndContents).map(
+//         ([file, contents]) => {
+//             const [, metadataStr] = contents.split('\n---')
+//             if (!metadataStr) return null
+//             const metadata = load(metadataStr.trim()) as ProblemMetadata
+//             return {
+//                 filename: file.name,
+//                 metadata,
+//                 createdAt: new Date(file.created_at);
+//                 modifiedAt: new Date(file.updated_at);
+//             }
+//         }
+//     )
+//     return problems.filter((problem) => !!problem)
+// }
+
+// getProblems().then((problems) => {
+//     const jsonData = JSON.stringify(problems)
+//     fs.writeFileSync(`${buildAssetsDirectory}/problems.json`, jsonData)
+// })
 
 fs.copyFile(
     __filename,
