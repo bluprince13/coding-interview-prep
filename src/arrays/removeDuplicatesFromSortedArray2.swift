d26730f1232501322c448f0dfc28679be090877c
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2024-12-25 22:41:41 +0000

    Add remove-duplicates-from-sorted-array-ii

--- src/arrays/removeDuplicatesFromSortedArray2.swift
+++ src/arrays/removeDuplicatesFromSortedArray2.swift
@@ -1,18 +1,22 @@
-// https://leetcode.com/problems/remove-duplicates-from-sorted-array
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii
 
 import Testing
 
-enum RemoveDuplicatesFromSortedArrayNamespace {
+enum RemoveDuplicatesFromSortedArray2Namespace {
     static func removeDuplicates(_ nums: inout [Int]) -> Int {
-        var write = 1
+        if nums.count <= 2 {
+            return nums.count
+        }
 
-        for read in 0 ..< nums.count {
-            if nums[read] != nums[write - 1] {
+        var write = 2
+
+        for read in 2 ..< nums.count {
+            if nums[read] != nums[write - 2] {
                 nums[write] = nums[read]
                 write += 1
             }
         }
-        
+
         return write
     }
 
@@ -30,8 +34,13 @@ enum RemoveDuplicatesFromSortedArrayNamespace {
         arguments: [
             TestData(
                 id: "1",
-                input: Input(nums: [1, 1, 2]),
-                expected: Expected(nums: [1, 2, 2], val: 2)
+                input: Input(nums: [1, 1, 1, 2, 2, 3]),
+                expected: Expected(nums: [1, 1, 2, 2, 3, 3], val: 5)
+            ),
+            TestData(
+                id: "2",
+                input: Input(nums: [0, 0, 1, 1, 1, 1, 2, 3, 3]),
+                expected: Expected(nums: [0, 0, 1, 1, 2, 3, 3, 3, 3], val: 7)
             )
         ]
     )
