8c6185f15b30f765b370460e4ce17eb4d5fc8699
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2025-01-11 22:01:00 +0000

    Add best-time-to-buy-and-sell-stock

---
+++ src/arrays/bestTimeToBuyAndSellStock.swift
@@ -0,0 +1,39 @@
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+
+import Testing
+
+enum BestTimeToBuyAndSellStockNamespace {
+    static func maxProfit(_ prices: [Int]) -> Int {
+        var minPrice = prices[0]
+        var maxProfit = 0
+        for price in prices {
+            minPrice = min(price, minPrice)
+            let profit = price - minPrice
+            maxProfit = max(profit, maxProfit)
+        }
+        return maxProfit
+    }
+
+    struct Input {
+        let nums: [Int]
+    }
+
+    struct Expected {
+        let val: Int
+    }
+
+    @Test(
+        "BestTimeToBuyAndSellStock",
+        arguments: [
+            TestData(
+                id: "1",
+                input: Input(nums: [7, 1, 5, 3, 6, 4]),
+                expected: Expected(val: 5)
+            )
+        ]
+    )
+    static func test(testData: TestData<Input, Expected>) {
+        var result = maxProfit(testData.input.nums)
+        #expect(result == testData.expected.val)
+    }
+}
