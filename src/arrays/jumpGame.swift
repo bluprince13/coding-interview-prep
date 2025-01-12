c251a12d2f969f3719d2c12e2419c8a73722bf04
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2025-01-12 14:26:50 +0000

    Add jumpGame

---
+++ src/arrays/jumpGame.swift
@@ -0,0 +1,52 @@
+// https://leetcode.com/problems/jump-game
+
+import Testing
+
+enum JumpGameNamespace {
+    static func canJump(_ nums: [Int]) -> Bool {
+        var maxReachableIndex = 0
+        let finalIndex = nums.count - 1
+
+        for (i, val) in nums.enumerated() where i <= maxReachableIndex {
+            maxReachableIndex = max(maxReachableIndex, i + val)
+            if maxReachableIndex >= finalIndex {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    struct Input {
+        let nums: [Int]
+    }
+
+    struct Expected {
+        let val: Bool
+    }
+
+    @Test(
+        "JumpGame",
+        arguments: [
+            TestData(
+                id: "1",
+                input: Input(nums: [2, 3, 1, 1, 4]),
+                expected: Expected(val: true)
+            ),
+            TestData(
+                id: "2",
+                input: Input(nums: [3, 2, 1, 0, 4]),
+                expected: Expected(val: false)
+            ),
+            TestData(
+                id: "3",
+                input: Input(nums: [3,0,8,2,0,0,1]),
+                expected: Expected(val: true)
+            )
+        ]
+    )
+    static func test(testData: TestData<Input, Expected>) {
+        let result = canJump(testData.input.nums)
+        #expect(result == testData.expected.val)
+    }
+}
