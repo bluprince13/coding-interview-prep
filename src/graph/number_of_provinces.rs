fe0d4c55d51c407dcdbcf9fe5689178bf584582c
Vipin Ajayakumar <vipinajayakumar@icloud.com>
2025-02-02 23:57:51 +0000

    Comment out incomplete file

--- src/graph/number_of_provinces.rs
+++ src/graph/number_of_provinces.rs
@@ -11,32 +11,32 @@ resources: []
 ---
 */
 
-pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
-    let visited = vec![false, is_connected.len()];
-    for i, city in is_connected.iter().enumerate() {
-        for j, _ in city.iter().enumerate() {
+// pub fn find_circle_num(is_connected: Vec<Vec<i32>>) -> i32 {
+//     let visited = vec![false, is_connected.len()];
+//     for i, city in is_connected.iter().enumerate() {
+//         for j, _ in city.iter().enumerate() {
 
-        }
-    }
-}
+//         }
+//     }
+// }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
 
-    #[test]
-    fn test_1() {
-        let is_connected = vec![vec![1, 1, 0], vec![1, 1, 0], vec![0, 0, 1]];
-        let actual = find_circle_num(is_connected);
+//     #[test]
+//     fn test_1() {
+//         let is_connected = vec![vec![1, 1, 0], vec![1, 1, 0], vec![0, 0, 1]];
+//         let actual = find_circle_num(is_connected);
 
-        assert_eq!(actual, 2);
-    }
+//         assert_eq!(actual, 2);
+//     }
 
-    #[test]
-    fn test_2() {
-        let is_connected = vec![vec![1, 0, 0], vec![0, 1, 0], vec![0, 0, 1]];
-        let actual = find_circle_num(is_connected);
+//     #[test]
+//     fn test_2() {
+//         let is_connected = vec![vec![1, 0, 0], vec![0, 1, 0], vec![0, 0, 1]];
+//         let actual = find_circle_num(is_connected);
 
-        assert_eq!(actual, 3);
-    }
-}
+//         assert_eq!(actual, 3);
+//     }
+// }
